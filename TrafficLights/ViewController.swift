c302a8e52fdc15668f46da42d0899e094d63691d
Aleksandr Kretov <kretov.1988@gmail.com>
2022-03-01 23:51:36 +0500

    Traffic lights ready

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var redLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let lights: [UIView] = [redLight, yellowLight, greenLight]
+        for light in lights {
+            light.layer.cornerRadius = light.frame.width / 2
+            light.alpha = 0.3
+        }
+        
+        
+        
     }
 
-
+    @IBAction func switchLight() {
+    }
+    
 }
 
