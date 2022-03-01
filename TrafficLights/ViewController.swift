816bcabf58e6f11a317a4b3ce2d0e81caf049a59
Aleksandr Kretov <kretov.1988@gmail.com>
2022-03-02 01:42:07 +0500

    late night build

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,47 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var redLight: UIView!
 
-    
-    @IBOutlet weak var greenLight: UIView!
-    @IBOutlet weak var yellowLight: UIView!
-    @IBOutlet weak var redLight: UIView!
+    @IBOutlet var switchButton: UIButton!
 
-    @IBOutlet weak var switchButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         let lights = [redLight, yellowLight, greenLight]
         for light in lights {
             light?.layer.cornerRadius = (light?.frame.width)! / 2
             light?.alpha = 0.3
         }
+
         switchButton.layer.cornerRadius = 16
         switchButton.setTitle("START", for: .normal)
     }
-    
-    var index = 0
 
-
+    var counter = 0
 
     @IBAction func switchButtonPressed(_ sender: Any) {
         switchButton.setTitle("NEXT", for: .normal)
         let lights = [redLight, yellowLight, greenLight]
-        switch lights[index] {
+        switch lights[counter] {
         case redLight:
             redLight.alpha = 1
             greenLight.alpha = 0.3
-            index += 1
+            counter = lights.index(after: counter)
         case yellowLight:
             yellowLight.alpha = 1
             redLight.alpha = 0.3
-            index += 1
+            counter = lights.index(after: counter)
         case greenLight:
             greenLight.alpha = 1
             yellowLight.alpha = 0.3
-            index = 0
+            counter = lights.startIndex
         default: break
         }
     }
 }
-
