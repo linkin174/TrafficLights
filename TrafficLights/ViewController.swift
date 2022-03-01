a64794586b697bdc5fe2e4d39344ac99e769f70d
Aleksandr Kretov <kretov.1988@gmail.com>
2022-03-02 01:18:40 +0500

    Traffic Lights working, but...

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,25 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var switchButton: UIButton!
+    
     @IBOutlet weak var greenLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var redLight: UIView!
+
+    @IBOutlet weak var switchButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let lights: [UIView] = [redLight, yellowLight, greenLight]
+        let lights = [redLight, yellowLight, greenLight]
         for light in lights {
-            light.layer.cornerRadius = light.frame.width / 2
-            light.alpha = 0.3
+            light?.layer.cornerRadius = (light?.frame.width)! / 2
+            light?.alpha = 0.3
         }
-        
-        
-        
-    }
-
-    @IBAction func switchLight() {
+        switchButton.layer.cornerRadius = 16
+        switchButton.setTitle("START", for: .normal)
     }
     
+    var index = 0
+
+
+
+    @IBAction func switchButtonPressed(_ sender: Any) {
+        switchButton.setTitle("NEXT", for: .normal)
+        let lights = [redLight, yellowLight, greenLight]
+        switch lights[index] {
+        case redLight:
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+            index += 1
+        case yellowLight:
+            yellowLight.alpha = 1
+            redLight.alpha = 0.3
+            index += 1
+        case greenLight:
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+            index = 0
+        default: break
+        }
+    }
 }
 
